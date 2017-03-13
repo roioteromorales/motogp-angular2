5492d4a12e8103c08337ef1602c43b6dd95a1e66
Roi Otero Morales <roioteromorales@gmail.com>
2017-03-13 23:24:54 +0100

    First conection with the api rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {CalendarService} from "./calendar.service";
+import {Calendar} from "./calendar";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CalendarService]
+
 })
-export class AppComponent {
-  title = 'Moto gp!';
+export class AppComponent implements OnInit {
+  calendar = new Calendar;
+
+  constructor(private calendarService: CalendarService) {
+  }
+
+  getCalendar(): void {
+    this.calendarService.getCalendar(2017)
+      .subscribe(calendar => this.calendar = calendar);
+  }
+
+  ngOnInit(): void {
+    this.getCalendar();
+  }
 }
---
+++ src/app/calendar.service.ts
@@ -0,0 +1,35 @@
+import {Calendar} from "./calendar";
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class CalendarService {
+  private baseUrl: string = 'http://localhost:8080/';
+
+  constructor(private http: Http) {
+  }
+
+  getCalendar(year: number): Observable<Calendar> {
+    return this.http
+      .get(this.baseUrl + 'calendar/' + year)
+      .map(mapCalendar)
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+
+  }
+}
+
+function mapCalendar(response: Response): Calendar {
+  return toCalendar(response.json());
+}
+
+function toCalendar(jsonResponse: any): Calendar {
+  let calendar = <Calendar>({
+    name: jsonResponse.name,
+    year: jsonResponse.year
+  });
+  console.log('Parsed Calendar:', calendar);
+  return calendar;
+}
+
+
